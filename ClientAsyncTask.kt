5c90aed9ba2beacd9531871b874a14fee12df0d7
mohreza115410176 <48266282+mohreza115410176@users.noreply.github.com>
2019-07-17 02:34:13 +0700

    Add files via upload

---
+++ ClientAsyncTask.kt
@@ -0,0 +1,55 @@
+package com.example.usergate.jadwalsholat
+
+import android.content.Context
+import android.os.AsyncTask
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+
+class ClientAsyncTask constructor(private val mContext: Context, postExecuteListener: On
+                                  AsyncTask<String, String, String>() {
+    val CONNECTON_TIMEOUT_MILLISECONDS = 60000
+    private val mPostExecuteListener : OnPostExecuteListener = postExecuteListener
+    interface OnPostExecuteListener {
+        fun onPostExecute(result: String)
+    }
+    override fun onPostExecute(result: String) {
+        super.onPostExecute(result)
+        if (mPostExecuteListener != null) {
+            mPostExecuteListener.onPostExecute(result)
+        }
+    }
+    override fun doInBackground(vararg urls: String?): String {
+        var urlConnection: HttpURLConnection? = nulltry {
+            val url = URL(urls[0])
+            urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.connectTimeout = CONNECTON_TIMEOUT_MILLISECONDS
+            urlConnection.readTimeout = CONNECTON_TIMEOUT_MILLISECONDS
+            var inString = streamToString(urlConnection.inputStream)
+            return inString
+        } catch (ex: Exception) {
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect()
+            }
+        }
+        return ""
+    }
+    fun streamToString(inputStream: InputStream): String {
+        val bufferReader = BufferedReader(InputStreamReader(inputStream))
+        var line: String
+        var result = ""
+        try {
+            do {
+                line = bufferReader.readLine()
+                if (line != null) {
+                    result += line
+                }
+            } while (line != null)
+            inputStream.close()
+        } catch (ex: Exception) {
+        }
+        return result
+    }
+}
---
+++ Kota.kt
@@ -0,0 +1,9 @@
+package com.example.usergate.jadwalsholat
+
+class Kota {
+    var id: Int? = null
+    var nama: String? = null
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
